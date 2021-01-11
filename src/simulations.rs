3a58e507394473091099f61d691f8207282e015c
TmLev <sololev27@gmail.com>
2021-01-11 09:15:32 +0300

    chore: bump main for external provider

--- src/simulations.rs
+++ src/simulations.rs
@@ -1,4 +1,20 @@
+use mist::{
+    Vm,
+    ExternalProvider,
+};
+
 fn main() {
     let vm = mist::Vm::default();
     println!("{:?}", vm);
+
+    let provider = ExternalProvider::new(
+        vec![
+            Vm::new(2, 1024),
+            Vm::new(4, 2048),
+            Vm::new(8, 2048),
+            Vm::new(8, 4096),
+        ]
+    );
+    println!("{:?}", provider);
+    println!("{:?}", provider.instance_types());
 }
