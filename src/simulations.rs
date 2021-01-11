242a1fd6f5b9a41416a17b7d031e5d3778a44dd9
TmLev <sololev27@gmail.com>
2021-01-11 10:06:55 +0300

    chore: bump main for service provider

--- src/simulations.rs
+++ src/simulations.rs
@@ -1,13 +1,14 @@
 use mist::{
     Vm,
     ExternalProvider,
+    ServiceProvider,
 };
 
 fn main() {
     let vm = mist::Vm::default();
     println!("{:?}", vm);
 
-    let provider = ExternalProvider::new(
+    let external_provider_1 = ExternalProvider::new(
         vec![
             Vm::new(2, 1024),
             Vm::new(4, 2048),
@@ -15,6 +16,22 @@ fn main() {
             Vm::new(8, 4096),
         ]
     );
-    println!("{:?}", provider);
-    println!("{:?}", provider.instance_types());
+    println!("{:?}", external_provider_1);
+    println!("{:?}", external_provider_1.instance_types());
+
+    let external_provider_2 = ExternalProvider::new(
+        vec![
+            Vm::new(2, 4096),
+            Vm::new(8, 8192),
+        ]
+    );
+
+    let service_provider = ServiceProvider::new(
+        vec![
+            &external_provider_1,
+            &external_provider_2,
+        ]
+    );
+    println!("{:?}", service_provider);
+    println!("{:?}", service_provider.public_cloud());
 }
