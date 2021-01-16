cfabfc88a736c8b484d38b31c70e9ce5e2c3f322
TmLev <sololev27@gmail.com>
2021-01-16 12:10:55 +0300

    feat: adjust example to actors

--- examples/provisioning.rs
+++ examples/provisioning.rs
@@ -1,37 +1,26 @@
+use actix::prelude::*;
+
 use mist::{
     Vm,
     ExternalProvider,
     ServiceProvider,
 };
 
-fn main() {
-    let vm = mist::Vm::default();
-    println!("{:?}", vm);
-
-    let external_provider_1 = ExternalProvider::new(
+#[actix_rt::main]
+async fn main() -> () {
+    let ep_addr = ExternalProvider::new(
         vec![
             Vm::new(2, 1024),
             Vm::new(4, 2048),
             Vm::new(8, 2048),
             Vm::new(8, 4096),
         ]
-    );
-    println!("{:?}", external_provider_1);
-    println!("{:?}", external_provider_1.instance_types());
+    ).start();
 
-    let external_provider_2 = ExternalProvider::new(
-        vec![
-            Vm::new(2, 4096),
-            Vm::new(8, 8192),
-        ]
-    );
+    let sp = ServiceProvider::new(vec![ep_addr.clone()]);
+    let res = sp.request().await;
+    println!("RESULT: {}", res);
+    assert_eq!(res, 124);
 
-    let service_provider = ServiceProvider::new(
-        vec![
-            &external_provider_1,
-            &external_provider_2,
-        ]
-    );
-    println!("{:?}", service_provider);
-    println!("{:?}", service_provider.public_cloud());
+    System::current().stop();
 }
