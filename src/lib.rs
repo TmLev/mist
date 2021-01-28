9a060638930f753a833357775f9f5649e3b9a663
TmLev <sololev27@gmail.com>
2021-01-28 23:17:11 +0300

    feat: re-export new types

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 pub(crate) mod vm;
+
 pub use crate::vm::Vm;
 
 pub(crate) mod providers;
-pub use crate::providers::{external::ExternalProvider, proto, service::ServiceProvider};
+
+pub use crate::providers::{
+    external::{Cost, ExternalProvider, InstanceType, InstanceTypes},
+    proto,
+    service::ServiceProvider,
+};
