1f5e94e78108d52741dcc6cb38735ed299b23c75
TmLev <sololev27@gmail.com>
2021-01-31 16:15:43 +0300

    feat: application with status

--- src/task.rs
+++ src/task.rs
@@ -1,6 +1,19 @@
-type Deadline = i64;
+pub type Deadline = i64;
 
 #[derive(Debug, Clone)]
 struct Task {
     deadline: Deadline,
+}
+
+pub type Tasks = Vec<Task>;
+
+enum ApplicationStatus {
+    Schedulable,
+    Unfeasible,
+}
+
+#[derive(Debug, Clone)]
+struct Application {
+    tasks: Tasks,
+    status: ApplicationStatus,
 }
