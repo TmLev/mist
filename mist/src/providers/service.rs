063d47648b9ab1f3e6707cda59ab1575fa9dbb97
TmLev <sololev27@gmail.com>
2021-02-10 21:44:10 +0300

    refactor: adjust service provider to stakker

--- mist/src/providers/service.rs
+++ mist/src/providers/service.rs
@@ -1,18 +1,31 @@
-use crate::proto::InstanceTypesRequest;
+use stakker::{call, ret_some_to, Actor, CX};
+
 use crate::providers::external::ExternalProvider;
 use crate::vms::InstanceTypes;
 
-#[derive(Debug)]
 pub struct ServiceProvider {
-    pub eps: Vec<Addr<ExternalProvider>>,
+    pub eps: Vec<Actor<ExternalProvider>>,
 }
 
 impl ServiceProvider {
-    pub fn new(eps: Vec<Addr<ExternalProvider>>) -> ServiceProvider {
-        ServiceProvider { eps }
+    pub fn init(cx: CX![], eps: Vec<Actor<ExternalProvider>>) -> Option<ServiceProvider> {
+        println!("SP: init start");
+        let ep = eps[0].clone();
+        println!("SP: before call!-ing request");
+        call!([cx], request_instance_types(ep));
+        println!("SP: after call!-ing request");
+        Some(Self { eps })
     }
 
-    pub async fn request_instance_types(&self, ep_addr: &Addr<ExternalProvider>) -> InstanceTypes {
-        ep_addr.send(InstanceTypesRequest(())).await.unwrap().0
+    pub fn request_instance_types(&self, cx: CX![], ep: Actor<ExternalProvider>) {
+        println!("SP: requesting instance types, before ret_to!");
+        let ret = ret_some_to!([cx], receive_instance_types() as (InstanceTypes));
+        println!("SP: after ret_to!, before call!");
+        call!([ep], instance_types(ret));
+        println!("SP: after call!");
+    }
+
+    pub fn receive_instance_types(&self, _cx: CX![], its: InstanceTypes) {
+        println!("{:?}", its);
     }
 }
