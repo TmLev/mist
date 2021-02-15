7e5f4c1308b594badbbf1409927682ad855e7075
TmLev <sololev27@gmail.com>
2021-02-15 09:15:01 +0300

    feat: declare vdb12 modules

---
+++ mist/src/algorithms/vdb12/mod.rs
@@ -0,0 +1,3 @@
+pub mod applications;
+pub mod schedulers;
+pub mod tasks;
--- mist/src/lib.rs
+++ mist/src/lib.rs
@@ -1,11 +1,5 @@
-pub(crate) mod vms;
-pub use crate::vms::{Cost, InstanceType, InstanceTypes, Vm};
-
-pub(crate) mod applications;
-pub(crate) mod tasks;
-
-pub(crate) mod providers;
-pub use crate::providers::{external::ExternalProvider, service::ServiceProvider};
+// Algorithms
+pub mod algorithms;
 
 // Currency for instance types
 pub use rusty_money;
