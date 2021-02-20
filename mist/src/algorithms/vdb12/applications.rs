3fcb9f8e57358315e343a2026eab7bde04d1e371
TmLev <sololev27@gmail.com>
2021-02-20 16:01:23 +0300

    feat: add (de-)serialize & default for application

--- mist/src/algorithms/vdb12/applications.rs
+++ mist/src/algorithms/vdb12/applications.rs
@@ -1,16 +1,47 @@
-use uuid;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 use crate::algorithms::vdb12::tasks::Task;
 
 #[derive(Debug, Clone)]
+#[derive(Deserialize, Serialize)]
 pub enum Status {
     Schedulable,
     Unfeasible,
 }
 
 #[derive(Debug, Clone)]
+#[derive(Deserialize, Serialize)]
 pub struct Application {
-    uuid: uuid::Uuid,
+    uuid: Uuid,
     status: Status,
     tasks: Vec<Task>,
+}
+
+impl Application {
+    pub fn new(uuid: Uuid, status: Status, tasks: Vec<Task>) -> Self {
+        Self {
+            uuid,
+            status,
+            tasks,
+        }
+    }
+
+    pub fn add_tasks(&mut self, tasks: Vec<Task>) {
+        self.tasks.extend(tasks);
+    }
+
+    pub fn to_json(&self) -> String {
+        serde_json::to_string_pretty(&self).unwrap()
+    }
+}
+
+impl Default for Application {
+    fn default() -> Self {
+        Self {
+            uuid: Uuid::new_v4(),
+            status: Status::Schedulable,
+            tasks: vec![],
+        }
+    }
 }
