576dfae54a3d062dcab01664bb6b02f63ca710af
TmLev <sololev27@gmail.com>
2021-02-20 16:10:39 +0300

    feat: add (de-)serialize & default for task

--- mist/src/algorithms/vdb12/tasks.rs
+++ mist/src/algorithms/vdb12/tasks.rs
@@ -1,6 +1,23 @@
-use std::time::Instant;
+use chrono::{DateTime, Utc};
+
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone)]
+#[derive(Deserialize, Serialize)]
 pub struct Task {
-    deadline: Instant,
+    deadline: DateTime<Utc>,
+}
+
+impl Task {
+    pub fn new(deadline: DateTime<Utc>) -> Self {
+        Self { deadline }
+    }
+}
+
+impl Default for Task {
+    fn default() -> Self {
+        Self {
+            deadline: Utc::now(),
+        }
+    }
 }
