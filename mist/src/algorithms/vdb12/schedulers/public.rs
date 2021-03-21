2f7e4610d774c49fdfea7da09a25d6a317b1a581
TmLev <sololev27@gmail.com>
2021-03-21 18:01:59 +0300

    feat: add public provider prototype

--- mist/src/algorithms/vdb12/schedulers/public.rs
+++ mist/src/algorithms/vdb12/schedulers/public.rs
@@ -1,1 +1,33 @@
+use crate::vdb12::{Application, PublicProvider, ScheduleCost};
 
+pub struct PublicScheduler {
+    public_providers: Vec<PublicProvider>,
+}
+
+impl PublicScheduler {
+    pub fn new(public_providers: Vec<PublicProvider>) -> Self {
+        Self { public_providers }
+    }
+
+    pub fn cheapest_public_provider(&self, application: &Application) -> &PublicProvider {
+        let possible_providers_with_costs: Vec<_> = self
+            .public_providers
+            .iter()
+            .filter_map(|provider| match provider.cost(application) {
+                ScheduleCost::Impossible => None,
+                ScheduleCost::Possible(cost) => Some((provider, cost)),
+            })
+            .collect();
+
+        if possible_providers_with_costs.len() == 0 {
+            // TODO(TmLev): public providers *should* be able to execute any application in theory.
+            panic!("Public providers can not execute application");
+        }
+
+        possible_providers_with_costs
+            .iter()
+            .min_by(|left, right| left.1.partial_cmp(&right.1).unwrap())
+            .unwrap()
+            .0
+    }
+}
