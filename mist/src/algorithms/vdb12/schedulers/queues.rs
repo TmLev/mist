dda260fb166cb0dc2f7aabda074c884a072a5e2f
TmLev <sololev27@gmail.com>
2021-03-22 10:12:49 +0300

    feat: implement some priority queues

---
+++ mist/src/algorithms/vdb12/schedulers/queues.rs
@@ -0,0 +1,85 @@
+use std::hash::Hash;
+
+use keyed_priority_queue::{Entry, KeyedPriorityQueue, KeyedPriorityQueueBorrowIter};
+
+use crate::vdb12::{Application, Deadline};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub trait PriorityQueue<Element, Priority>
+where
+    Element: Hash + Eq,
+    Priority: Ord,
+{
+    fn push(&mut self, element: Element, priority: Priority) -> Option<Priority>;
+    fn pop(&mut self) -> Option<(Element, Priority)>;
+    fn len(&self) -> usize;
+    fn iter(&self) -> KeyedPriorityQueueBorrowIter<TKey, TPriority, S>;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct EarliestDeadlineFirst {
+    queue: KeyedPriorityQueue<Application, Deadline>,
+}
+
+impl EarliestDeadlineFirst {
+    pub fn new() -> Self {
+        Self {
+            queue: KeyedPriorityQueue::new(),
+        }
+    }
+}
+
+impl PriorityQueue<Application, Deadline> for EarliestDeadlineFirst {
+    fn push(&mut self, element: Application, priority: Deadline) -> Option<Deadline> {
+        self.queue.push(element, priority)
+    }
+
+    fn pop(&mut self) -> Option<(Application, Deadline)> {
+        self.queue.pop()
+    }
+
+    fn len(&self) -> usize {
+        self.queue.len()
+    }
+
+    fn iter(&self) -> KeyedPriorityQueueBorrowIter<TKey, TPriority, S> {
+        self.queue.iter()
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct FirstComeFirstServed {
+    queue: KeyedPriorityQueue<Application, usize>,
+    counter: usize,
+}
+
+impl FirstComeFirstServed {
+    pub fn new() -> Self {
+        Self {
+            queue: KeyedPriorityQueue::new(),
+            counter: 0,
+        }
+    }
+}
+
+impl PriorityQueue<Application, usize> for FirstComeFirstServed {
+    fn push(&mut self, element: Application, priority: usize) -> Option<usize> {
+        self.counter += 1;
+        self.queue.push(element, self.counter)
+    }
+
+    fn pop(&mut self) -> Option<(Application, usize)> {
+        self.queue.pop()
+    }
+
+    fn len(&self) -> usize {
+        self.queue.len()
+    }
+
+    fn iter(&self) -> KeyedPriorityQueueBorrowIter<TKey, TPriority, S> {
+        self.queue.iter()
+    }
+}
