366ccad5b0a8f08d52b05e8c64405ca416221cca
TmLev <sololev27@gmail.com>
2021-03-28 23:44:35 +0300

    refactor: move tasks to applications

--- mist/src/algorithms/vdb12/applications.rs
+++ mist/src/algorithms/vdb12/applications.rs
@@ -1,36 +1,57 @@
+use std::cmp::Ordering;
+
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::vdb12::Task;
+use crate::vdb12::Vm;
+
+#[derive(Debug, Clone)]
+#[derive(Deserialize, Serialize)]
+pub struct Task {
+    pub minimal_vm_requirements: Vm,
+}
+
+pub type Deadline = DateTime<Utc>;
 
 #[derive(Debug, Clone)]
 #[derive(Deserialize, Serialize)]
 pub struct Application {
-    pub uuid: Uuid,
+    // Bag of tasks.
     pub tasks: Vec<Task>,
+    // Way to differentiate applications.
+    uuid: Uuid,
+    // Deadline to meet.
+    // Private because it may be used as key in sorted data structures.
+    deadline: Deadline,
 }
 
 impl Application {
-    pub fn new(uuid: Uuid, tasks: Vec<Task>) -> Self {
-        Self { uuid, tasks }
-    }
-
-    pub fn add_tasks(&mut self, tasks: Vec<Task>) {
-        self.tasks.extend(tasks);
-        // TODO(TmLev): What? Scheduler should sort applications, but what about tasks?
-        self.tasks.sort();
-    }
-
-    pub fn to_json(&self) -> String {
-        serde_json::to_string_pretty(&self).unwrap()
+    pub fn new(uuid: Uuid, tasks: Vec<Task>, deadline: Deadline) -> Self {
+        Self {
+            uuid,
+            tasks,
+            deadline,
+        }
     }
 }
 
-impl Default for Application {
-    fn default() -> Self {
-        Self {
-            uuid: Uuid::new_v4(),
-            tasks: vec![],
-        }
+impl PartialEq for Application {
+    fn eq(&self, other: &Self) -> bool {
+        self.deadline == other.deadline
+    }
+}
+
+impl Eq for Application {}
+
+impl PartialOrd for Application {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.deadline.partial_cmp(&other.deadline)
+    }
+}
+
+impl Ord for Application {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.deadline.cmp(&other.deadline)
     }
 }
