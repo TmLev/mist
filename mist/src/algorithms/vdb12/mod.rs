d77f013ab7fd0003bb8178249c347a32c749e033
TmLev <sololev27@gmail.com>
2021-04-08 10:35:00 +0300

    refactor: rename vms to virtual machines

--- mist/src/algorithms/vdb12/mod.rs
+++ mist/src/algorithms/vdb12/mod.rs
@@ -1,9 +1,10 @@
 mod applications;
+mod customers;
 mod providers;
 mod schedulers;
-mod vms;
+mod virtual_machines;
 
 pub use applications::{Application, Deadline, Task};
 pub use providers::{PublicProvider, ScheduleCost};
 pub use schedulers::policies;
-pub use vms::{BillingInterval, Cost, InstanceType, Price, Vm};
+pub use virtual_machines::{BillingInterval, Cost, InstanceType, Price, Vm};
