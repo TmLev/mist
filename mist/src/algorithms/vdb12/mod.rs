4876439f550615638c0b79448305f59a0c0f6699
TmLev <sololev27@gmail.com>
2021-04-13 15:47:11 +0300

    chore: bump lib declarations

--- mist/src/algorithms/vdb12/mod.rs
+++ mist/src/algorithms/vdb12/mod.rs
@@ -5,9 +5,13 @@ mod schedulers;
 mod virtual_machines;
 
 pub use applications::{Application, Deadline, Task};
+pub use customers::Customer;
 pub use providers::{
     public::{PublicProvider, ScheduleCost},
     service::ServiceProvider,
 };
-pub use schedulers::{hybrid::HybridScheduler, public::PublicScheduler};
+pub use schedulers::{
+    hybrid::{HybridScheduler, SortingPolicy, UnfeasiblePolicy},
+    public::PublicScheduler,
+};
 pub use virtual_machines::{BillingInterval, Cost, InstanceType, Price, Vm};
