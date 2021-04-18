d016dc48d8822b16e33cadc9c40282a9e051bbf7
TmLev <sololev27@gmail.com>
2021-04-18 21:07:11 +0300

    feat: scan & schedule

--- mist/src/algorithms/vdb12/providers/service.rs
+++ mist/src/algorithms/vdb12/providers/service.rs
@@ -1,21 +1,49 @@
+use std::time::Duration;
+
 use log;
 
-use stakker::CX;
+use stakker::{after, CX};
 
 use crate::vdb12::{Application, HybridScheduler};
 
 pub struct ServiceProvider {
     scheduler: HybridScheduler,
+    scan_interval: Duration,
+    schedule_interval: Duration,
 }
 
 impl ServiceProvider {
-    pub fn init(_cx: CX![], scheduler: HybridScheduler) -> Option<Self> {
-        log::info!("Initializing");
-        Some(Self { scheduler })
+    pub fn init(cx: CX![], scheduler: HybridScheduler) -> Option<Self> {
+        log::info!("Initialising");
+
+        // TODO(TmLev): add customization.
+        let scan_interval = Duration::from_secs(5);
+        let schedule_interval = Duration::from_secs(1);
+
+        after!(scan_interval, [cx], scan());
+        after!(schedule_interval, [cx], schedule());
+
+        Some(Self {
+            scheduler,
+            scan_interval,
+            schedule_interval,
+        })
     }
 
     pub fn customer_request(&mut self, _cx: CX![], applications: Vec<Application>) {
         log::info!("Customer request arrived");
         self.scheduler.add_applications(applications);
+    }
+
+    fn scan(&mut self, cx: CX![]) {
+        log::info!("Scanning application queue");
+        self.scheduler.scan(cx.now());
+        after!(self.scan_interval, [cx], scan());
+    }
+
+    fn schedule(&mut self, cx: CX![]) {
+        log::info!("Scheduling applications");
+        self.scheduler.schedule(cx.now());
+        after!(self.schedule_interval, [cx], schedule());
     }
 }
