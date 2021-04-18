8e4301cd2a00f2efdfb2e464e8728299d9fe6c21
TmLev <sololev27@gmail.com>
2021-04-18 21:22:44 +0300

    feat: apply unfeasible policy

--- mist/src/algorithms/vdb12/schedulers/hybrid.rs
+++ mist/src/algorithms/vdb12/schedulers/hybrid.rs
@@ -1,20 +1,30 @@
-use crate::vdb12::{Application, InstanceType};
+use std::time::Instant;
 
+use crate::vdb12::{Application, InstanceType, PublicScheduler};
+
+/// Sorting policy determines in what order applications waiting in queue should be scheduled.
 pub enum SortingPolicy {
+    /// FIFO order.
     FirstComeFirstServed,
+    /// Prioritise applications with the earliest deadline.
     EarliestDeadlineFirst,
 }
 
+/// Unfeasible application policy.
 pub enum UnfeasiblePolicy {
+    /// Find the cheapest application before the unfeasible one and delegate it.
     CheapestToPublic,
+    /// Send the unfeasible application to the public cloud.
     UnfeasibleToPublic,
 }
 
+/// Hybrid cloud scheduler. Distributes arriving applications between the private and public clouds.
 pub struct HybridScheduler {
     private_instance_types: Vec<InstanceType>,
     application_queue: Vec<Application>,
     sorting_policy: SortingPolicy,
     unfeasible_policy: UnfeasiblePolicy,
+    public_scheduler: PublicScheduler,
 }
 
 impl HybridScheduler {
@@ -22,18 +32,28 @@ impl HybridScheduler {
         private_instance_types: Vec<InstanceType>,
         sorting_policy: SortingPolicy,
         unfeasible_policy: UnfeasiblePolicy,
+        public_scheduler: PublicScheduler,
     ) -> Self {
         Self {
             private_instance_types,
             application_queue: Vec::new(),
             sorting_policy,
             unfeasible_policy,
+            public_scheduler,
         }
     }
 
     pub fn add_applications(&mut self, applications: Vec<Application>) {
         self.application_queue.extend(applications);
         self.sort_queue();
+    }
+
+    fn remove_application_from_queue(&mut self, to_remove: &Application) -> Application {
+        let application = to_remove.clone();
+        // `Vec::retain` preserves order of elements, no need to sort.
+        self.application_queue
+            .retain(|application| application != to_remove);
+        application
     }
 
     fn sort_queue(&mut self) {
@@ -45,16 +65,30 @@ impl HybridScheduler {
         };
     }
 
-    fn scan(&mut self) -> Option<Application> {
+    pub fn scan(&mut self, now: Instant) {
         for (index, application) in self.application_queue.iter_mut().enumerate() {
-            for task in application.tasks.iter() {
-                // let time = get_start_time(schedule, task);
-                // let it = get_instance_type(task, time);
-            }
+            let status = { for task in application.tasks.iter() {} };
         }
-
-        None
     }
 
-    fn apply_policy(&mut self, unfeasible_application: &Application) {}
+    pub fn schedule(&mut self, now: Instant) {}
+
+    fn apply_unfeasible_policy(&mut self, unfeasible_application: &Application) {
+        let unfeasible_application = self.remove_application_from_queue(unfeasible_application);
+
+        match self.unfeasible_policy {
+            UnfeasiblePolicy::UnfeasibleToPublic => {
+                let mut public_provider = self
+                    .public_scheduler
+                    .cheapest_public_provider(&unfeasible_application);
+                match public_provider {
+                    None => {
+                        // TODO(TmLev): application deadline can not be met.
+                    }
+                    Some(mut public_provider) => public_provider.schedule(unfeasible_application),
+                }
+            }
+            UnfeasiblePolicy::CheapestToPublic => {}
+        }
+    }
 }
