93d169532208351fa4356b9dbc87115f5b9c8229
TmLev <sololev27@gmail.com>
2021-04-26 20:22:27 +0300

    chore: bump lib declarations

--- mist/src/algorithms/hetj18/mod.rs
+++ mist/src/algorithms/hetj18/mod.rs
@@ -1,4 +1,6 @@
+mod context;
 mod jobs;
+mod metrics;
 mod nodes;
 mod pools;
 mod predictions;
@@ -7,6 +9,9 @@ mod resources;
 mod users;
 
 pub use jobs::{Deadline, Job, Task};
+pub use metrics::Metrics;
 pub use nodes::Node;
 pub use pools::{private::PrivatePool, public::PublicPool};
+pub use providers::ServiceProvider;
 pub use resources::Resource;
+pub use users::User;
