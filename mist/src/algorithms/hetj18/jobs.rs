6871c6658ba74f8a10bdd25660a5f513419cdf2a
TmLev <sololev27@gmail.com>
2021-04-26 14:55:27 +0300

    feat: add uuid

--- mist/src/algorithms/hetj18/jobs.rs
+++ mist/src/algorithms/hetj18/jobs.rs
@@ -1,10 +1,14 @@
 use chrono::{DateTime, Utc};
+use std::cmp::Ordering;
+use std::ops::Deref;
+use uuid::Uuid;
 
 pub struct Task {}
 
 pub type Deadline = DateTime<Utc>;
 
 pub struct Job {
+    uuid: Uuid,
     tasks: Vec<Task>,
     deadline: Deadline,
 }
@@ -12,5 +16,9 @@ pub struct Job {
 impl Job {
     pub fn generate() -> Self {
         todo!()
+    }
+
+    pub fn uuid(&self) -> Uuid {
+        self.uuid
     }
 }
