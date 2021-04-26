bbddcd380cb2f75ed8fc7e6b4e7d2cd70ed4f688
TmLev <sololev27@gmail.com>
2021-04-26 16:21:22 +0300

    feat: introduce service provider

---
+++ mist/src/algorithms/hetj18/providers.rs
@@ -0,0 +1,55 @@
+use stakker::{after, call, CX};
+
+use crate::hetj18::{Job, PrivatePool, PublicPool};
+
+pub struct ServiceProvider {
+    private_pool: PrivatePool,
+    public_cloud: Vec<PublicPool>,
+}
+
+impl ServiceProvider {
+    pub fn init(
+        _cx: CX![],
+        private_pool: PrivatePool,
+        public_cloud: Vec<PublicPool>,
+    ) -> Option<Self> {
+        log::info!("Initialising");
+        Some(Self {
+            private_pool,
+            public_cloud,
+        })
+    }
+
+    pub fn user_request(&mut self, cx: CX![], mut jobs: Vec<Job>) {
+        log::info!(
+            "Received user request, job uuids: {}",
+            jobs.iter()
+                .map(|job| job.uuid().to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        );
+
+        priority_sort(&mut jobs);
+
+        for job in jobs.iter() {
+            if self.private_pool.try_schedule(job) {
+                continue;
+            }
+
+            if self.try_public_schedule(job) {
+                continue;
+            }
+        }
+    }
+
+    fn try_public_schedule(&mut self, job: &Job) -> bool {
+        self.public_cloud
+            .iter()
+            .find(|&public_pool| public_pool.try_schedule(job))
+            .is_some()
+    }
+}
+
+fn priority_sort(jobs: &mut Vec<Job>) {
+    jobs.sort_by_key(|job| job.deadline());
+}
