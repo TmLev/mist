087fb2f97cd0893ea01c6a368fd1eb1c5cc91634
TmLev <sololev27@gmail.com>
2021-04-26 13:53:06 +0300

    feat: add deadline

--- mist/src/algorithms/hetj18/jobs.rs
+++ mist/src/algorithms/hetj18/jobs.rs
@@ -1,9 +1,14 @@
+use chrono::{DateTime, Utc};
+
 use crate::hetj18::Resource;
 
 pub struct Task {}
 
+pub type Deadline = DateTime<Utc>;
+
 pub struct Job {
-    intensity: Resource,
+    tasks: Vec<Task>,
+    deadline: Deadline,
 }
 
 impl Job {}
